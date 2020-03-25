e453551f97dc08e5fed620c1e06c71addd54c08d
Vincent Mutune <VMutune@STANDARDMEDIA.CO.KE>
2020-03-25 15:13:47 +0300

    add seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,9 +12,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
-<<<<<<< HEAD
-=======
+
         $this->call(ContentSeeder::class);
->>>>>>> saved my code
+
     }
 }
