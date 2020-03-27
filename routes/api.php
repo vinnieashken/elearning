ec17fe720a0d3e69382d68c482d1a9c0e398f143
Vincent Mutune <VMutune@STANDARDMEDIA.CO.KE>
2020-03-27 15:04:27 +0300

    changed payment routs

--- routes/api.php
+++ routes/api.php
@@ -31,8 +31,8 @@ Route::get('/modules/subject/{id}','Api\ModulesController@getSubjectModules');
 Route::get('/questions/module/{id}','Api\QuestionsController@getModuleQuestions');
 
 Route::get('/payments/subscriptions','Api\PaymentsController@getSubscriptions');
-Route::get('/payments/subscribe','Api\PaymentsController@startTransaction'); //post
-Route::get('/payments/mpesa/stk','Api\PaymentsController@startMpesaStkPush');
+Route::post('/payments/subscribe','Api\PaymentsController@startTransaction'); //post
+Route::post('/payments/mpesa/stk','Api\PaymentsController@startMpesaStkPush');
 Route::post('/payments/mpesa/callback','Api\PaymentsController@mpesaCallback');
 
 
