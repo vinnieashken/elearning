91e6cd577b1014f2c251c5e9643a81d050754cce
Dennis Joel Mwagiru <dennismwagiru@gmail.com>
2020-03-30 13:21:30 +0300

    update - add apis to app

--- resources/app/components/profile.jsx
+++ resources/app/components/profile.jsx
@@ -1,6 +1,43 @@
 import React, { useEffect, useState } from 'react';
+import {API, ENV} from "../common/constants";
+import {ClipLoader} from "react-spinners";
+import {Link} from "react-router-dom";
+import moment from "moment";
+import ToolkitProvider, {Search} from "react-bootstrap-table2-toolkit";
+import BootstrapTable from "react-bootstrap-table-next";
+const { SearchBar } = Search;
 
 export default function (props) {
+    const [loading, setLoading] = useState(true);
+    const [message, setMessage] = useState(false);
+    const [messageType, setMessageType] = useState( '');
+    const [response, setResponse] = useState('');
+    const [subscriptions, setSubscriptions] = useState([]);
+
+    useEffect((e) => {
+            getSubscription();
+    }, []);
+
+    const getSubscription = () => {
+        $.ajax({
+            url: `${API}/payments/subscriptions/user/${props.user.id}`,
+            method: 'GET',
+            error: function (xhr, status, error) {
+                var response = JSON.parse(xhr['responseText'])['message'];
+                if (xhr.status === 405)
+                    response = "Sorry an error has occurred. We are working on it. (405)";
+                setLoading(false);
+                setMessage(true);
+                setMessageType('alert alert-danger');
+                setResponse(response);
+            }.bind(this),
+            success: function (res) {
+                setSubscriptions(res);
+                setLoading(false);
+            }.bind(this)
+        })
+    };
+
     return (
         <React.Fragment>
             <div className="page-title">
@@ -49,12 +86,49 @@ export default function (props) {
                             </div>
                         </div>
                         <div className="card-body">
-                            <div className="row">
-                                <div className="col-4 text-center">
-                                    <span className="h5 mb-0">86</span>
-                                    <span className="d-block text-sm">Finished Exams</span>
-                                </div>
-                            </div>
+                           <div>
+                               {
+
+                                   loading ?
+                                       <div className="text-center mt-4">
+                                           <ClipLoader color={'#cf2027'} />
+                                       </div> :
+
+                                       <div className="row">
+                                           <div className="col-md-12">
+                                               <ToolkitProvider
+                                                   keyField="id"
+                                                   data={ subscriptions }
+                                                   columns={
+                                                       [
+                                                           {dataField: 'ordernumber',      text: 'Order',    sort: true},
+                                                           {dataField: 'receipt',        text: 'Receipt',      sort: true},
+                                                           {dataField: 'method',        text: 'Method',      sort: true},
+                                                           {dataField: 'package',   text: 'Package',      sort: true},
+                                                           {dataField: 'startdate',   text: 'Start Date',      sort: true},
+                                                           {dataField: 'expiry_date',   text: 'Expiry Date',      sort: true},
+                                                       ]
+                                                   } search={true}>
+                                                   {
+                                                       props =>
+                                                           (
+                                                               <React.Fragment>
+                                                                   <div className='row  mb-3'>
+                                                                       <div className='col-md-12'>
+                                                                           <SearchBar className='col-md-4 float-right mb-3' { ...props.searchProps } />
+                                                                       </div>
+                                                                   </div>
+                                                                   <BootstrapTable { ...props.baseProps } wrapperClasses="table-responsive"/>
+
+                                                               </React.Fragment>
+                                                           )
+                                                   }
+                                               </ToolkitProvider>
+                                           </div>
+                                       </div>
+
+                               }
+                           </div>
                         </div>
                         <div className="card-footer">
 
