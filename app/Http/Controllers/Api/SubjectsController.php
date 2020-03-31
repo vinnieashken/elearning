7ff1fdd72808d4f3365883c5677e70f397420a3c
Vincent Mutune <VMutune@STANDARDMEDIA.CO.KE>
2020-03-31 14:39:21 +0300

    added thumburl field on subjects

--- app/Http/Controllers/Api/SubjectsController.php
+++ app/Http/Controllers/Api/SubjectsController.php
@@ -50,7 +50,7 @@ class SubjectsController extends Controller
             $page = $request->page;
 
             $results =  $model->where('class_id',$classid)->where('class_id',$classid)->leftJoin('classes','subjects.class_id','=','classes.id')
-                ->select('subjects.id','subjects.class_id','subjects.subject','classes.class')->paginate($size)->items();
+                ->select('subjects.id','subjects.class_id','subjects.subject','subjects.thumburl','classes.class')->paginate($size)->items();
 
             $totalrecords = $model->where('class_id',$classid)->count();
             $totalpages = ceil($totalrecords / $size);
@@ -67,6 +67,6 @@ class SubjectsController extends Controller
             return $data;
         }
         return $model->where('class_id',$classid)->leftJoin('classes','subjects.class_id','=','classes.id')
-            ->select('subjects.id','subjects.class_id','subjects.subject','classes.class')->get();
+            ->select('subjects.id','subjects.class_id','subjects.subject','subjects.thumburl','classes.class')->get();
     }
 }
