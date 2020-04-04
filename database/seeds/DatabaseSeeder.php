0074e55aec831e144d099366ddbf14e1f1a739c2
caydee <caydee2010@aol.com>
2020-04-04 14:09:20 +0300

    seed

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,8 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(UsersTableSeeder::class);
-
-        $this->call(QuestionSeeder::class);
+        #$this->call(QuestionSeeder::class);
 
     }
 }
