5eff695025568c33c156b73d721f97f6e87bfb1b
git <VMutune@STANDARDMEDIA.CO.KE>
2020-04-15 12:43:26 +0300

    added institution, teacher, student register apis

---
+++ database/migrations/2020_04_15_071756_create_institutions_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateInstitutionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('institutions', function (Blueprint $table) {
+            $table->id();
+            $table->integer('vas_id')->unique();
+            $table->string('email');
+            $table->string('name');
+            $table->string('address')->nullable();
+            $table->string('phone')->nullable();
+            $table->integer('active')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('institutions');
+    }
+}
