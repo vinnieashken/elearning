d7a5e197a8236a9f83c5bd6e14aedd4178395195
Dennis Joel Mwagiru <dennismwagiru@gmail.com>
2020-04-22 13:10:37 +0300

    update - Website Redesign

--- routes/web.php
+++ routes/web.php
@@ -57,7 +57,7 @@ Route::post('cms/edituserroles','Cms@edituserroles');
 Route::post('cms/delete','Cms@delete');
 Route::post('cms/payments','Datatable@payments');
 Route::get('{any}', function ($any = null) {
-    return view('app');
+    return view('newApp');
 })->where('any', '^((?!cms|assets|static|get_users|get_classes|get_subjects|get_modules|get_questions|subjectfromclass|choices|get_rates|admin|register|login|api|questionanswers|admin|upload).)*$');
 
 
