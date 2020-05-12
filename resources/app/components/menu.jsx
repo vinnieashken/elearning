0c5e910b79506bb558acf6299ff4c1bc332b8ace
Dennis Joel Mwagiru <dennismwagiru@gmail.com>
2020-05-12 15:51:46 +0300

    update - Design Changes

--- resources/app/components/menu.jsx
+++ resources/app/components/menu.jsx
@@ -286,7 +286,7 @@ export default function (props) {
                             {/*</script>*/}
 
 
-                            <div className="collapse navbar-collapse flex-column " id="navbar" style={{background: 'red'}}>
+                            <div className="collapse navbar-collapse flex-column " id="navbar" style={{background: '#1865f1'}}>
 
 
                                 <ul className="navbar-nav justify-content-around w-90">
