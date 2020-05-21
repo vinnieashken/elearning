7e36514be9165cb70025e978e3a08ff9da045f75
Dennis Joel Mwagiru <dennismwagiru@gmail.com>
2020-05-21 17:56:08 +0300

    update - Tawkto

--- resources/views/newApp.blade.php
+++ resources/views/newApp.blade.php
@@ -98,6 +98,20 @@
         }
 
     </style>
+    <!--Start of Tawk.to Script-->
+    <script type="text/javascript">
+        var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+        (function(){
+            var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+            s1.async=true;
+            s1.src='https://embed.tawk.to/5ec67dda6f7d401ccbb8a970/default';
+            s1.charset='UTF-8';
+            s1.setAttribute('crossorigin','*');
+            s0.parentNode.insertBefore(s1,s0);
+        })();
+    </script>
+    <!--End of Tawk.to Script-->
+
 </head>
 <body>
 <div class="main h-100 w-100" id="app" >
