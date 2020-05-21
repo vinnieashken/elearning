f75a6d5537797242c3d0fc4024a4fb4c97e7f93d
Dennis Joel Mwagiru <dennismwagiru@gmail.com>
2020-05-21 08:14:44 +0300

    update - Allow everyone free access

--- resources/app/common/actions.js
+++ resources/app/common/actions.js
@@ -2,26 +2,27 @@ import { SUBSCRIPTION_LOADED, LOADING_SUBSCRIPTION, SUBJECTS_LOADED, CLASSES_LOA
 
 export function fetchSubscription(user) {
     return function (dispatch) {
-        if (user.email && ['dkarimi@standardmedia.co.ke','mshahi@standardmedia.co.ke', 'lkinyanjui@standardmedia.co.ke', 'dennismwagiru@ymail.com', 'mbuthiaw001@gmail.com'].includes((user.email.toLowerCase()))) {
-            dispatch ({ type: SUBSCRIPTION_LOADED, payload: {id: 1} });
-        } else if(parseInt(user.institution_id) === 11 && (parseInt(user.owner) === 1 || parseInt(user.teacher) === 1)) {
-            dispatch ({ type: SUBSCRIPTION_LOADED, payload: {id: 1} });
-        } else {
-            dispatch({type: LOADING_SUBSCRIPTION, payload: true});
-            return $.ajax({
-                // url: `${API}/payments/subscription/user/2`,
-                url: `${API}/payments/subscription/user/${user.user_id}`,
-                method: 'get',
-                dataType: 'json',
-                error: function (xhr, status, error) {
-                    dispatch({type: LOADING_SUBSCRIPTION, payload: false});
-                }.bind(this),
-                success: function (res) {
-                    console.log(res);
-                    dispatch({type: SUBSCRIPTION_LOADED, payload: res});
-                }.bind(this)
-            })
-        }
+        dispatch ({ type: SUBSCRIPTION_LOADED, payload: {id: 1} });
+        // if (user.email && ['dkarimi@standardmedia.co.ke','mshahi@standardmedia.co.ke', 'lkinyanjui@standardmedia.co.ke', 'dennismwagiru@ymail.com', 'mbuthiaw001@gmail.com'].includes((user.email.toLowerCase()))) {
+        //     dispatch ({ type: SUBSCRIPTION_LOADED, payload: {id: 1} });
+        // } else if(parseInt(user.institution_id) === 11 && (parseInt(user.owner) === 1 || parseInt(user.teacher) === 1)) {
+        //     dispatch ({ type: SUBSCRIPTION_LOADED, payload: {id: 1} });
+        // } else {
+        //     dispatch({type: LOADING_SUBSCRIPTION, payload: true});
+        //     return $.ajax({
+        //         // url: `${API}/payments/subscription/user/2`,
+        //         url: `${API}/payments/subscription/user/${user.user_id}`,
+        //         method: 'get',
+        //         dataType: 'json',
+        //         error: function (xhr, status, error) {
+        //             dispatch({type: LOADING_SUBSCRIPTION, payload: false});
+        //         }.bind(this),
+        //         success: function (res) {
+        //             console.log(res);
+        //             dispatch({type: SUBSCRIPTION_LOADED, payload: res});
+        //         }.bind(this)
+        //     })
+        // }
     };
 }
 
