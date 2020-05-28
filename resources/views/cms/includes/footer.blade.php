e88b4188eebed980c2c77fcab0517ecc24f0b66c
Dennis Kiptoo Kiptugen <dennis.kiptoo@caydeesoft.org>
2020-05-28 12:18:51 +0300

    customers

--- resources/views/cms/includes/footer.blade.php
+++ resources/views/cms/includes/footer.blade.php
@@ -13,9 +13,9 @@
 </footer>
 </div>
 </div>
-<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+
 <script src="{{ asset('assets/js/app.js') }}"></script>
-<script src="//cdn.datatables.net/1.10.21/js/jquery.dataTables.min.js"></script>
+
 <script src="{{ asset('assets/summernote/summernote-bs4.min.js') }}"></script>
 @include('cms.includes.js')
 </body>
