084aac61e588284f6d5de68c334467329ac17d76
Dennis Joel Mwagiru <dennismwagiru@gmail.com>
2020-07-17 00:03:55 +0300

    update - Cookie consent

--- resources/views/vendor/cookieConsent/dialogContents.blade.php
+++ resources/views/vendor/cookieConsent/dialogContents.blade.php
@@ -1,11 +1,11 @@
 <div class="alert bg-black js-cookie-consent cookie-consent text-center">
 
     <span class="text-white cookie-consent__message">
-        {!! trans('cookieConsent::texts.message') !!}
+        To personalise content, tailor ads and provide best user experience, we use cookies. By using our site, you agree to use our cookies.
     </span>
 
     <button class="btn cookie-btn js-cookie-consent-agree cookie-consent__agree">
-        {{ trans('cookieConsent::texts.agree') }}
+        Accept & Continue
     </button>
 
 </div>
