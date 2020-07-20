a36482f49301576f27bb9fef47a2e6f8422b2737
git <VMutune@STANDARDMEDIA.CO.KE>
2020-07-20 15:04:50 +0300

    changed usermodules api

--- app/Http/Controllers/Api/ModulesController.php
+++ app/Http/Controllers/Api/ModulesController.php
@@ -603,7 +603,7 @@ class ModulesController extends Controller
             ->get();
 
         $choiceless = Choiceless::where('user_id',$userid)->select('module_id')->distinct()->get()->pluck('module_id')->toArray();
-//        $choiceless = Module::whereIn('id',$choiceless)
+        $choiceless = Module::whereIn('id',$choiceless)->get();
 //            ->leftJoin('subjects','subjects.id','=','modules.subject_id')
 //            ->leftJoin('classes','classes.id','=','subjects.class_id')
 //            ->select('modules.id','modules.module','user_answers_choiceless.created_at as date','subjects.id as subject_id','subjects.subject','classes.id as class_id','classes.class')
