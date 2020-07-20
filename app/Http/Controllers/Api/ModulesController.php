5ed584e000e48554f5480e73b6b48932f58957f6
git <VMutune@STANDARDMEDIA.CO.KE>
2020-07-20 14:59:38 +0300

    changed usermodules api

--- app/Http/Controllers/Api/ModulesController.php
+++ app/Http/Controllers/Api/ModulesController.php
@@ -602,8 +602,8 @@ class ModulesController extends Controller
             ->select('modules.id','modules.module','user_answers.created_at as date','subjects.id as subject_id','subjects.subject','classes.id as class_id','classes.class','marks.score','marks.questions','marks.percentage')
             ->get();
 
-        $choiceless = Choiceless::where('user_id',$userid)->distinct('user_answers_choiceless.module_id')
-            ->leftJoin('modules','modules.id','=','user_answers_choiceless.module_id')
+        $choicelessids = Choiceless::where('user_id',$userid)->select('module_id')->distinct()->get()->pluck('module_id')->toArray();
+        $choiceless = Module::whereIn('id',$choicelessids)
             ->leftJoin('subjects','subjects.id','=','modules.subject_id')
             ->leftJoin('classes','classes.id','=','subjects.class_id')
             ->select('modules.id','modules.module','user_answers_choiceless.created_at as date','subjects.id as subject_id','subjects.subject','classes.id as class_id','classes.class')
