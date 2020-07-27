3da10a9f575d2e811dfbd422cf61007e355754b3
git <VMutune@STANDARDMEDIA.CO.KE>
2020-07-27 15:48:13 +0300

    added stats

--- resources/views/reports/statistics.blade.php
+++ resources/views/reports/statistics.blade.php
@@ -7,6 +7,16 @@
     <div>
         <div>
            {{ dd($data) }}
+            @foreach($data as $Key => $value)
+                <div>
+                    <p>{{ $key }}</p>
+                    <ul>
+                    @foreach($value as $Keys => $values)
+                       <li>{{ $values[0] }}  {{ $values[1] }}</li>
+                    @endforeach
+                    </ul>
+                </div>
+            @endforeach
         </div>
     </div>
 
