61131dd9a8e04c0db9b1d1240cb5585c004dc79a
git <VMutune@STANDARDMEDIA.CO.KE>
2020-07-30 14:02:22 +0300

    added lessons apis

--- app/Http/Controllers/Api/LessonsController.php
+++ app/Http/Controllers/Api/LessonsController.php
@@ -24,6 +24,56 @@ class LessonsController extends Controller
 
     public function getUnits(Request $request)
     {
+
+        if($request->has('page') && $request->has('size'))
+        {
+            $size = $request->size;
+
+            if($request->has('class') && $request->has('subject'))
+            {
+                $class = $request->class;
+                $subject = $request->subject;
+
+                $units = Unit::where('class',$class)->where('subject',$subject)->paginate($size)->items();
+
+                return $units;
+            }
+            if($request->has('class'))
+            {
+                $class = $request->class;
+                $units = Unit::where('class',$class)->paginate($size)->items();
+                return $units;
+            }
+            if($request->has('subject'))
+            {
+                $subject = $request->subject;
+                $units = Unit::where('subject',$subject)->paginate($size)->items();
+                return $units;
+            }
+        }
+
+        if($request->has('class') && $request->has('subject'))
+        {
+            $class = $request->class;
+            $subject = $request->subject;
+
+            $units = Unit::where('class',$class)->where('subject',$subject)->get();
+
+            return $units;
+        }
+        if($request->has('class'))
+        {
+            $class = $request->class;
+            $units = Unit::where('class',$class)->get();
+            return $units;
+        }
+        if($request->has('subject'))
+        {
+            $subject = $request->subject;
+            $units = Unit::where('subject',$subject)->get();
+            return $units;
+        }
+
         $units = Unit::all();
         return $units;
     }
