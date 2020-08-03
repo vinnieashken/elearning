7e15644305574ecafe2f28aca0dd9e68e183f51c
git <VMutune@STANDARDMEDIA.CO.KE>
2020-08-03 15:23:44 +0300

    added modules report

--- app/Http/Controllers/Api/ModulesController.php
+++ app/Http/Controllers/Api/ModulesController.php
@@ -951,9 +951,12 @@ class ModulesController extends Controller
         {
             $page = $request->page;
             $size = $request->size;
-            $modules = Module::with('options')->paginate($size)->items();
+            $modules = Module::paginate($size)->items();
+            $scores = AnswerSheet::where('module_id',10)
+                ->leftjoin('user_answers','user_answers.question_id','=','answers.question_id')
+                ->count();
 
-            return $modules;
+            return $scores;
         }
 
     }
